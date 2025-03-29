2b92dbb92b48911317e8bf3f7dda2e7f6e624da6
ROHIT <kumarohit1202@gmail.com>
2025-03-29 09:16:02 +0530

    program.cs changes

--- DataCollection_Eval/Program.cs
+++ DataCollection_Eval/Program.cs
@@ -9,17 +9,8 @@ namespace DataCollection_Eval
 {
     static class Program
     {
-        /// <summary>
-        /// The main entry point for the application.
-        /// </summary>
         static void Main()
         {
-            //ServiceBase[] ServicesToRun;
-            //ServicesToRun = new ServiceBase[] 
-            //{ 
-            //    new Service1() 
-            //};
-            //ServiceBase.Run(ServicesToRun);
 
 #if (!DEBUG)
             CultureInfo culture = CultureInfo.CreateSpecificCulture("en");
